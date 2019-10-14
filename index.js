0951b662ef8b161e5ba9e8ea0fb4d3eeb52fc7a8
MichaelBaynon <baynonmichael@gmail.com>
2019-10-14 13:56:03 -0400

    middleware

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ server.get("/", (req, res) => {
 server.post("/api/register", (req, res) => {
   let user = req.body;
 
-  const hash = bcrypt.hashSync(user.password, 12)
+  const hash = bcrypt.hashSync(user.password, 12);
 
-  user.password = hash
+  user.password = hash;
 
   Users.add(user)
     .then(saved => {
@@ -37,7 +37,8 @@ server.post("/api/login", (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: "Invalid Credentials" });
@@ -48,7 +49,7 @@ server.post("/api/login", (req, res) => {
     });
 });
 
-server.get("/api/users", (req, res) => {
+server.get("/api/users", protected,  (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
@@ -58,10 +59,28 @@ server.get("/api/users", (req, res) => {
 
 server.get("/hash", (req, res) => {
   // read a password from the Authorization header
-  // return an object with the password hashed using bcryptjs
-  // { hash: '970(&(:OHKJHIY*HJKH(*^)*&YLKJBLKJGHIUGH(*P' }
-  
+  const password = req.headers.authorization;
+
+  if (password) {
+    // return an object with the password hashed using bcryptjs
+    const hash = bcrypt.hashSync(password, 12);
+    // { hash: '970(&(:OHKJHIY*HJKH(*^)*&YLKJBLKJGHIUGH(*P' }
+    res.status(200).json({ hash });
+  } else {
+    res.status(400).json({ message: "please provide credentials" });
+  }
 });
+
+function protected(req, res, next) {
+  const {username, password} = req.headers
+
+  if(username && password) {
+
+  } else {
+    res.status(400).json({message: 'please provide credentials'})
+  }
+
+}
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
